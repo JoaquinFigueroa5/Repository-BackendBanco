9f87f0986840a02ac3248d01ac2d9773975300b9
JoaquinFigueroa5 <hfigueroa-2023417@kinal.edu.gt>
2025-06-03 22:23:05 -0600

    Backend initialized

---
+++ configs/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from 'cors';
+import helmet from "helmet";
+import morgan from "morgan";
+import { dbConnection } from "./mongo.js";
+import limiter from "../src/middlewares/validate-cant-request.js";
+
+const middlewares = (app) => {
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+    app.use(cors());
+    app.use(helmet());
+    app.use(morgan('dev'));
+    app.use(limiter)
+}
+
+const routes = (app) => {
+
+}
+
+const conectarDB = async () => {
+    try {
+        await dbConnection();
+        console.log('Successful connection to the database');
+    } catch (error) {
+        console.log('Failed to connect to database');
+    }
+}
+
+export const initServer = async () => {
+    const app = express();
+    const port = process.env.PORT || 3000;
+    try {
+        middlewares(app);
+        conectarDB();
+        routes(app);
+        app.listen(port);
+        console.log(`server running on port ${port}`)
+    } catch (error) {
+        console.log(`server init failed: ${error}`);
+    }
+}
